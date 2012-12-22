9f38ece8de38d0ae2f6caacdf0bbd1b6aa554a34
Eric Prunier <eric.prunier@gmail.com>
2012-12-22 12:31:02 +0100

    Project URLs corrections

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-webapp-template/lein-template "1.0.0"
   :description "A Leiningen template for Web apps based on Compojure, Hiccup, Bootstrap and jQuery"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/eprunier/lein-webapp-template"
   :license {:name "Eclipse Public License"
-            :url "https://github.com/eprunier/lein-webapp-template"}
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
