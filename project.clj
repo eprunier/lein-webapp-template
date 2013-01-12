ef616ab7b2ddb138a79b7e792498a78ce3e950e4
Eric Prunier <eric.prunier@gmail.com>
2013-01-12 11:59:42 +0100

    Version 1.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-webapp-template/lein-template "1.1.0-SNAPSHOT"
+(defproject lein-webapp-template/lein-template "1.1.0"
   :description "A Leiningen template for Web apps based on Compojure, Hiccup, Bootstrap and jQuery"
   :url "https://github.com/eprunier/lein-webapp-template"
   :license {:name "Eclipse Public License"
