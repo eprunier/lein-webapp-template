c4de8e8dfccc533e42ebe2520917d158da422844
Eric Prunier <eric.prunier@gmail.com>
2013-02-26 22:03:44 +0100

    Add put! function for flash

--- src/leiningen/new/lein_webapp_template/clj/util/flash.clj
+++ src/leiningen/new/lein_webapp_template/clj/util/flash.clj
@@ -2,6 +2,11 @@
   (:refer-clojure :exclude [get])
   (:require [{{name}}.middleware.session :as session-manager]))
 
+(defn put!
+  "Put key/value in flash"
+  [k v]
+  (session-manager/flash-put! k v))
+
 (defn get
   "Retrieve a flash value"
   [k]
