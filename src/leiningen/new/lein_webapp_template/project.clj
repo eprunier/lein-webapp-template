fdce709a5840516fd3e9b2c9c576153b42a035a0
Eric Prunier <eric.prunier@gmail.com>
2013-03-03 23:09:47 +0100

    Update to clojure 1.5

--- src/leiningen/new/lein_webapp_template/project.clj
+++ src/leiningen/new/lein_webapp_template/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [ring "1.1.6"]
                  [compojure "1.1.3"]
                  [stencil "0.3.2"]]
