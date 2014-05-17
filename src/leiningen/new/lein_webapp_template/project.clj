6a934bee617077054d190ce8806bcf67ec05119e
Eric Prunier <eric.prunier@gmail.com>
2014-05-17 16:46:17 +0200

    Update dependencies

--- src/leiningen/new/lein_webapp_template/project.clj
+++ src/leiningen/new/lein_webapp_template/project.clj
@@ -5,12 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring "1.2.2"]
-                 [compojure "1.1.3"]
-                 [stencil "0.3.2"]
-                 [clj-http "0.7.3"]
-                 [cheshire "5.2.0"]]
+                 [compojure "1.1.8"]
+                 [stencil "0.3.3"]
+                 [clj-http "0.9.1"]
+                 [cheshire "5.3.1"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
                    :source-paths ["dev"]}}
-  :plugins [[lein-ring "0.8.5"]]
+  :plugins [[lein-ring "0.8.10"]]
   :ring {:handler {{name}}.app/site-handler}
   :main {{name}})
